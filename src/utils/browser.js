f099bfcbe1f0ed6063dbbf57bce8b153be4f9dd8
IBMpay <108310162+IBMpay@users.noreply.github.com>
2023-02-27 14:48:34 +0300

    Update browser.js

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,4 +1,7 @@
-//判断是否IE<11浏览器
+// ترجم النصوص التوضيحية للعربية
+
+// يتم استخدام هذه الدوال لتحديد إذا كان المستخدم يستخدم Internet Explorer أو Edge
+
 export function isIE() {
   return navigator.userAgent.indexOf('compatible') > -1 && navigator.userAgent.indexOf('MSIE') > -1;
 }
@@ -7,27 +10,28 @@ export function isIE11() {
   return navigator.userAgent.indexOf('Trident') > -1 && navigator.userAgent.indexOf('rv:11.0') > -1;
 }
 
-//判断是否IE的Edge浏览器
 export function isEdge() {
   return navigator.userAgent.indexOf('Edge') > -1 && !isIE();
 }
 
+// تحديد إصدار Internet Explorer في حالة العثور عليه
+
 export function getIEVersion() {
-  let userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
-  let isIE = isIE();
-  let isIE11 = isIE11();
-  let isEdge = isEdge();
+  const userAgent = navigator.userAgent;
+  const isIE = isIE();
+  const isIE11 = isIE11();
+  const isEdgeBrowser = isEdge();
 
   if (isIE) {
-    let reIE = new RegExp('MSIE (\\d+\\.\\d+);');
+    const reIE = new RegExp('MSIE (\\d+\\.\\d+);');
     reIE.test(userAgent);
-    let fIEVersion = parseFloat(RegExp['$1']);
+    const fIEVersion = parseFloat(RegExp['$1']);
     if (fIEVersion === 7 || fIEVersion === 8 || fIEVersion === 9 || fIEVersion === 10) {
       return fIEVersion;
     } else {
-      return 6; //IE版本<7
+      return 6; // إصدارات Internet Explorer التي يقل عن 7
     }
-  } else if (isEdge) {
+  } else if (isEdgeBrowser) {
     return 'edge';
   } else if (isIE11) {
     return 11;
